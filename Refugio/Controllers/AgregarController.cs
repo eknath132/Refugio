76836e38fa0c21e1e462bb1c2f9d7e71b6bc5662
renzo <perales.azarel@gmail.com>
2021-06-08 19:34:06 -0300

    Usuarios

---
+++ Refugio/Controllers/AgregarController.cs
@@ -0,0 +1,65 @@
+﻿using Refugio.Models;
+using Refugio.Models.SessionLoguin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Refugio.Controllers
+{
+    public class AgregarController : Controller
+    {
+        // GET: Agregar
+        public ActionResult Index()
+        {
+            List<ListSession> list;
+            using (refugioEntities db = new refugioEntities())
+            {
+                list = (from d in db.Adm
+                        select new ListSession
+                        {
+                            ID = d.ID,
+                            USUARIO = d.USUARIO,
+                        }).ToList();
+            }
+            return View(list);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        public ActionResult SessionAdd(string user, string pass)
+        {
+            string ePass = Encrypt.GetSHA256(pass);
+
+            try
+            {
+                using (refugioEntities db = new refugioEntities())
+
+                {
+                    if (ModelState.IsValid)
+                    {
+                        
+                            var oUser = new Adm();
+                            oUser.ID = 2; 
+                            oUser.USUARIO = user;
+                            oUser.PASS = ePass;
+                            db.Adm.Add(oUser);
+                            db.SaveChanges();
+
+                        
+                        return Content("Usuario Agregado");
+
+                    }
+                    return Content("~/Agregar/Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrio un error" + ex.Message);
+            }
+        }
+    }
+}
